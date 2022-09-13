d9c3f39eccf9c0e255d8e47283a21e7fcd6810dc
Jay Tin <jaytintran@gmail.com>
2022-09-13 23:14:18 +0200

    finished day 16

---
+++ day-016-object-oriented-programming.py
@@ -0,0 +1,17 @@
+"""
+At some point in the project, you will may have coded a ton of lines and tons of functions, each of them related to one another. 
+Bascially your code tries to do a lot of things, and manage a whole bunch of relationships. And because of that, at some point your code become very spaghetti-looking-like.
+Annd at this point it's very hard to keep track and remember whatever is going on inside your code.
+This style of programming called Procedural Programming. Where we set up procedures or functions that do particular things. And one procedure leads to another, all in all from top to bottom.
+Procedual Programming is the most easiest paradigms of Programming. But it's lack the ability to be comprehensive for the big picture, the complexity, and as our programs grow, it will get very confusing.
+To write and grow more and more complex projects we need to use the OOP or Object Oriented Programming.
+"""
+
+from turtle import Turtle, Screen
+
+tommy = Turtle()
+print(tommy)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+
